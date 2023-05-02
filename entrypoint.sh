b05210e84a667da4618ecc9548300518fb24fc96
Kevin Funke <funke@validierung.cc>
2023-05-02 19:04:17 +0200

    small improvement; changed default rpc user name

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,22 +12,16 @@ fi
 
 if [ ! -e "${EMARK_CONF}" ]; then
   tee -a >${EMARK_CONF} <<EOF
-
 server=1
-
-rpcuser=${EMARK_RPCUSER:-emark}
+rpcuser=${EMARK_RPCUSER:-emarkrpc}
 rpcpassword=${EMARK_RPCPASSWORD}
-
 rpcclienttimeout=${EMARK_RPCCLIENTTIMEOUT:-30}
-printtoconsole=1
-addnode=199.247.2.54
-addnode=65.109.141.173
 EOF
 echo "Created new configuration at ${EMARK_CONF}"
 fi
 
 if [ $# -eq 0 ]; then
-  /emark/eMark/src/eMarkd -rpcbind=:4444 -rpcallowip=*
+  /emark/eMark/src/eMarkd -rpcbind=:4444 -rpcallowip=* -printtoconsole=1
 else
   exec "$@"
 fi
